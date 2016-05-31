2534a77109d095198f8c6a463f44e797faa5580a
Bobby Craig <robtcraig@gmail.com>
2016-05-30 21:17:42 -0400

    Switching directories

--- posts/change_visited_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "../config.php";
-    require "../functions/functions.php";
-    session_start();
-?>
-
-<?php
-    dbQuery("UPDATE event SET visited = " . $_POST['dad'] . " WHERE event_id = " . $_SESSION['event_id'] . ";");
-?>
