e66730e3d1abda8e28164bd201ca6f734c0e4154
Bobby Craig <robtcraig@gmail.com>
2016-07-24 21:40:22 -0400

    Removed material design files because of git glitch. Will readd in next commit

--- MD PHP Solution/include/which-styles.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="/css/main.css">
